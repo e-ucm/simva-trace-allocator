cab7781b3b4537fba7eb29adbe445421aed3598c
Synpheros <Synpheros@hotmail.com>
2020-10-07 23:26:12 +0200

    Added config for topics directory

--- config.js
+++ config.js
@@ -9,10 +9,11 @@ config.minio.useSSL = (process.env.MINIO_SSL === "true") || true
 config.minio.port = process.env.MINIO_PORT || (config.minio.useSSL ? 443 : 80)
 config.minio.accessKey = process.env.MINIO_ACCESS_KEY || 'root'
 config.minio.secretKey = process.env.MINIO_SECRET_KEY || 'password'
-config.minio.bucket = process.env.MINIO_BUCKET || 'datalake'
+config.minio.bucket = process.env.MINIO_BUCKET || 'traces'
+config.minio.topics_dir = process.env.MINIO_TOPICS_DIR || 'kafka-topics'
 
 config.simva = {}
-config.simva.host = process.env.SIMVA_HOST || 'api.simva.e-ucm.es'
+config.simva.host = process.env.SIMVA_HOST || 'simva-api.simva.e-ucm.es'
 config.simva.protocol = process.env.SIMVA_PROTOCOL || 'https'
 config.simva.port = process.env.SIMVA_PORT || '443'
 config.simva.url = config.simva.protocol + '://' + config.simva.host + ':' + config.simva.port;
